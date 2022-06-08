b293d9c53d8ece17a5c5ef5cdf61cbebecadd3b6
Ozurumba Chinedu Francis <76830829+cloudtechhills@users.noreply.github.com>
2022-06-09 00:37:21 +0100

    Updated: Search By Country and Name of School

--- universityAPI.py
+++ universityAPI.py
@@ -1,15 +1,16 @@
 import requests
 from time import sleep
 
-#Version 1.0
+#Version 1.1 Upgraded
 #This program utilizes the beauty of universities.hipolabs.com API to get information
 #about different Universities all around the world.add()
 
-print("This Program Fetches Universities Data by Country")
+print("This Program Fetches Universities Data by Country and Name of Institution")
 print("--------------------------------")
 country = str(input("Enter Name of Country to Search: "))
+name = str(input("Enter Name of the University: "))
 
-reqUrl = f"http://universities.hipolabs.com/search?country={country}"
+reqUrl = f"http://universities.hipolabs.com/search?country={country}&name={name}"
 
 
 response = requests.get(reqUrl)
@@ -21,6 +22,7 @@ for i in response.json():
     domains = i['domains']
     web_pages = i['web_pages']
     name = i['name']
-    print(f"University Name: {name}\nUniversity Domain Name: {domains}\nUniversity Official Website: {web_pages}\n")
+    country = i['country']
+    print(f"University Name: {name}\nUniversity Domain Name: {domains}\nUniversity Official Website: {web_pages}\nCountry Located In: {country}\n")
     
     
